bca1754a16393c3d5dfbe23d141cd181b3fad148
aliwekaiq <shapsinfo@gmail.com>
2023-01-19 23:19:56 +0000

    git ignore

--- test.py
+++
@@ -1,9 +0,0 @@
-import openpyxl
-
-wb = openpyxl.load_workbook('/home/ali/Desktop/Talktalk/colored_cell.xlsx')
-sheet = wb.get_sheet_by_name("Resources by squad")
-
-
-color = sheet['J221'].fill.start_color.index
-
-print(color)
